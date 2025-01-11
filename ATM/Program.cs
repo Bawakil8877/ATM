773f7f4ffed3651acbb864b30d9ec672ffef4ba7
Bawakil8877 <07026981622mustapha@gmail.com>
2025-01-11 11:12:26 -0800

    Handling exception

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,73 +1,81 @@
-﻿using  System;
+﻿using System;
 using System.Collections.Generic;
 
 public class cardHolder
 {
-    string cardNum;
-    int pin;
-    string firstName;
-    string lastName;
-    double balance;
+    public string CardNum { get; private set; }
+    public int Pin { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public double Balance { get; private set; }
 
     public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
     {
-        this.cardNum = cardNum;
-        this.pin = pin;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.balance = balance;
+        CardNum = cardNum;
+        Pin = pin;
+        FirstName = firstName;
+        LastName = lastName;
+        Balance = balance;
     }
 
-    public string getCardNum() => cardNum;
-    public int getPin() => pin;
-    public string getFirstName() => firstName;
-    public string getLastName() => lastName;
-    public double getBalance() => balance;
+    public void SetBalance(double newBalance) => Balance = newBalance;
+}
 
-    public void setPin(int newPin) => pin = newPin;
-    public void setFirstName(string newFirstName) => firstName = newFirstName;
-    public void setLastName(string newLastName) => lastName = newLastName;
-    public void setBalance(double newBalance) => balance = newBalance;
-
-    public static void Main(string[] args)
+public class Program
+{
+    static void PrintOptions()
     {
-        void printOptions()
-        {
-            Console.WriteLine("\nPlease choose from the following options:");
-            Console.WriteLine("1. Deposit");
-            Console.WriteLine("2. Withdraw");
-            Console.WriteLine("3. Show Balance");
-            Console.WriteLine("4. Exit");
-        }
+        Console.WriteLine("\nPlease choose from the following options:");
+        Console.WriteLine("1. Deposit");
+        Console.WriteLine("2. Withdraw");
+        Console.WriteLine("3. Show Balance");
+        Console.WriteLine("4. Exit");
+    }
 
-        void deposit(cardHolder currentUser)
+    static void Deposit(cardHolder currentUser)
+    {
+        try
         {
             Console.WriteLine("How much $$ would you like to deposit: ");
             double depositAmount = double.Parse(Console.ReadLine());
-            currentUser.setBalance(currentUser.getBalance() + depositAmount);
-            Console.WriteLine("Thank you for your deposit. Your new balance is: $" + currentUser.getBalance());
+            currentUser.SetBalance(currentUser.Balance + depositAmount);
+            Console.WriteLine("Thank you for your deposit. Your new balance is: $" + currentUser.Balance);
         }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a numeric value.");
+        }
+    }
 
-        void withdraw(cardHolder currentUser)
+    static void Withdraw(cardHolder currentUser)
+    {
+        try
         {
             Console.WriteLine("How much $$ would you like to withdraw: ");
             double withdrawalAmount = double.Parse(Console.ReadLine());
-            if (withdrawalAmount > currentUser.getBalance())
+            if (withdrawalAmount > currentUser.Balance)
             {
                 Console.WriteLine("Insufficient balance.");
             }
             else
             {
-                currentUser.setBalance(currentUser.getBalance() - withdrawalAmount);
-                Console.WriteLine("You're good to go! Your new balance is: $" + currentUser.getBalance());
+                currentUser.SetBalance(currentUser.Balance - withdrawalAmount);
+                Console.WriteLine("You're good to go! Your new balance is: $" + currentUser.Balance);
             }
         }
-
-        void checkBalance(cardHolder currentUser)
+        catch (FormatException)
         {
-            Console.WriteLine("Current balance: $" + currentUser.getBalance());
+            Console.WriteLine("Invalid input. Please enter a numeric value.");
         }
+    }
 
+    static void CheckBalance(cardHolder currentUser)
+    {
+        Console.WriteLine("Current balance: $" + currentUser.Balance);
+    }
+
+    public static void Main(string[] args)
+    {
         List<cardHolder> list = new List<cardHolder>
         {
             new cardHolder("4532772818527395", 1234, "John", "Griffith", 150.31),
@@ -79,7 +87,7 @@ public class cardHolder
         Console.WriteLine("Welcome to the ATM!");
         Console.WriteLine("Please insert your debit card: ");
         string debitCardNum = Console.ReadLine();
-        cardHolder currentUser = list.Find(user => user.getCardNum() == debitCardNum);
+        cardHolder currentUser = list.Find(user => user.CardNum == debitCardNum);
 
         if (currentUser == null)
         {
@@ -88,43 +96,60 @@ public class cardHolder
         }
 
         Console.WriteLine("Please enter your PIN: ");
-        int userPin = int.Parse(Console.ReadLine());
-
-        if (currentUser.getPin() != userPin)
+        try
         {
-            Console.WriteLine("Incorrect PIN. Exiting...");
+            int userPin = int.Parse(Console.ReadLine());
+            if (currentUser.Pin != userPin)
+            {
+                Console.WriteLine("Incorrect PIN. Exiting...");
+                return;
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid PIN format. Exiting...");
             return;
         }
 
-        Console.WriteLine($"Welcome, {currentUser.getFirstName()}!");
+        Console.WriteLine($"Welcome, {currentUser.FirstName}!");
 
         int option;
         do
         {
-            printOptions();
-            option = int.Parse(Console.ReadLine());
-
-            switch (option)
+            PrintOptions();
+            try
             {
-                case 1:
-                    deposit(currentUser);
-                    break;
-                case 2:
-                    withdraw(currentUser);
-                    break;
-                case 3:
-                    checkBalance(currentUser);
-                    break;
-                case 4:
-                    Console.WriteLine("Thank you! Have a nice day.");
-                    break;
-                default:
-                    Console.WriteLine("Invalid option. Please try again.");
-                    break;
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Deposit(currentUser);
+                        break;
+                    case 2:
+                        Withdraw(currentUser);
+                        break;
+                    case 3:
+                        CheckBalance(currentUser);
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you! Have a nice day.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid option. Please enter a number between 1 and 4.");
+                option = 0; // Keeps the loop running in case of invalid input.
             }
         } while (option != 4);
     }
+
+
 }
+
 
 
 
